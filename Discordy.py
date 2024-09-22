b4acaaa631b0ef11b6966a27606c505d2fbad1c4
krarktel112 <krarktel@gmail.com>
2024-09-22 11:46:56 -0400

    Update Discordy.py

--- Discordy.py
+++ Discordy.py
@@ -2,10 +2,30 @@ import discord
 import asyncio
 import datetime
 
-client = discord.Client()
 
-@client.event()
-async def on_ready():
-    print("Bot is online")
+class DiscordClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        discord.Client.__init__(self, **kwargs)
 
-client.login()
+    @asyncio.coroutine
+    def on_ready(self):
+        servers = list(self.servers)
+        for server in servers:
+            if server.name == 'My server':
+                break
+
+        for channel in server.channels:
+            if channel.name == 'general':
+                break
+
+        now = datetime.datetime.now()
+        yield from self.send_message(channel, 'Api Success! at ' + str(now))
+        print('Success!')
+        yield from self.close()
+
+
+if __name__ == '__main__':
+    dc = DiscordClient()
+    email = input('email : ')
+    password = input('password : ')
+    dc.run(email, password)
