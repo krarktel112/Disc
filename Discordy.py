91d442d6a6c53b04854645d3461560f750bc5c8f
krarktel112 <krarktel@gmail.com>
2024-09-22 12:49:39 -0400

    Update Discordy.py

--- Discordy.py
+++ Discordy.py
@@ -7,6 +7,7 @@ class DiscordClient(discord.Client):
     def __init__(self, *args, **kwargs):
         discord.Client.__init__(self, **kwargs)
 
+    @asyncio.coroutine
     def on_ready(self):
         servers = list(self.servers)
         for server in servers:
