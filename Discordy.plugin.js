85057cebced992846e32a964f47b931c1da5cbdf
krarktel112 <krarktel@gmail.com>
2024-10-13 15:26:38 -0400

    Update Discordy.plugin.js

--- Discordy.plugin.js
+++ Discordy.plugin.js
@@ -1,28 +1,15 @@
-/**
- * @name FriendNotifications
- * @author DevilBro
- * @authorId 278543574059057154
- * @version 2.0.1
- * @description Shows a Notification when a Friend or a User, you choose to observe, changes their Status
- * @invite Jx3TjNS
- * @donate https://www.paypal.me/MircoWittrien
- * @patreon https://www.patreon.com/MircoWittrien
- * @website https://mwittrien.github.io/
- * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/FriendNotifications/
- * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Plugins/FriendNotifications/FriendNotifications.plugin.js
- */
 function statusupdate(name) {
 	const { exec } = require('child_process');
 	// Open a new command prompt window
 	exec('start cmd');
 	process.stdout.write("cd Backup\n");
 	console.log(`Hello, ${name}!`);
-	offline;
-	idle;
-	dnd;
-	gaming;
-	streaming;
-	screensharing;
+	process.stdout.write("python3 STonline.py\n")
+	process.stdout.write("python3 STidle.py\n")
+	process.stdout.write("python3 STdnd.py\n")
+	process.stdout.write("python3 STgaming.py\n")
+	process.stdout.write("python3 STstreaming.py\n")
+	process.stdout.write("python3 STscreensharing.py\n")
 }
 
 module.exports = (_ => {
