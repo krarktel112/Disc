2b0a03dc5e4c2fd6fc233c0cc310c9b6a2025cc1
krarktel112 <krarktel@gmail.com>
2024-10-13 18:03:43 -0400

    Update Discordy.plugin.js

--- Discordy.plugin.js
+++ Discordy.plugin.js
@@ -1,3 +1,16 @@
+/**
+ * @name FriendNotifications
+ * @author DevilBro
+ * @authorId 278543574059057154
+ * @version 2.0.1
+ * @description Shows a Notification when a Friend or a User, you choose to observe, changes their Status
+ * @invite Jx3TjNS
+ * @donate https://www.paypal.me/MircoWittrien
+ * @patreon https://www.patreon.com/MircoWittrien
+ * @website https://mwittrien.github.io/
+ * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/FriendNotifications/
+ * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Plugins/FriendNotifications/FriendNotifications.plugin.js
+ */
 function statusupdate(name) {
 	const { exec } = require('child_process');
 	// Open a new command prompt window
