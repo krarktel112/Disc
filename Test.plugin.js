2bf50abeb46a605a013442c383e0150a47d49e70
krarktel112 <krarktel@gmail.com>
2024-10-14 16:02:38 -0400

    Update Test.plugin.js

--- Test.plugin.js
+++ Test.plugin.js
@@ -1,16 +1,20 @@
 /**
- * @name Test
- * @author DevilBro
- * @authorId 278543574059057154
- * @version 2.0.1
- * @description Shows a Notification when a Friend or a User, you choose to observe, changes their Status
- * @invite Jx3TjNS
- * @donate https://www.paypal.me/MircoWittrien
- * @patreon https://www.patreon.com/MircoWittrien
- * @website https://mwittrien.github.io/
- * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/FriendNotifications/
- * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Plugins/FriendNotifications/FriendNotifications.plugin.js
+ * @name Discordy 
+ * @author YourName
+ * @description Describe the basic functions. Maybe a support server link.
+ * @version 0.0.1
  */
-const { exec } = require('child_process');
-	// Open a new command prompt window
-exec('start cmd');
+
+module.exports = class MyPlugin {
+  constructor(meta) {
+    // Do stuff in here before starting
+  }
+
+  start() {
+    // Do stuff when enabled
+  }
+
+  stop() {
+    // Cleanup when disabled
+  }
+};
