d37369f97447b13f94d65a1a6119e7f709ba372a
krarktel112 <krarktel@gmail.com>
2024-11-04 17:38:16 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -8,7 +8,8 @@ async def fetch_user_info(bot, user_id):
     print(f"User: {user.name}#{user.discriminator}")
     print(f"ID: {user.id}")
     print(f"Bot: {user.bot}")
-    print(user.presence)
+    attributes = dir(user)
+    print(attributes)
     
 intents = discord.Intents.default()
 intents.members = True
