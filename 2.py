4905b07b528b28ab4042d8805a07307f2429aa88
krarktel112 <krarktel@gmail.com>
2024-11-04 17:52:59 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,7 +5,7 @@ TOKEN = input("Token:")
 
 async def fetch_user_info(bot, user_id):
     user = await bot.fetch_user(user_id)
-    user2 = bot.fetch_user(user_id)
+    user2 = bot.get_user(user_id)
     print(f"User: {user.name}#{user.discriminator}")
     print(f"ID: {user.id}")
     print(f"Bot: {user.bot}")
