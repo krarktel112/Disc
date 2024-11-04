f7ebfef4e177b85dcab798e07c941fa7d264a6a9
krarktel112 <krarktel@gmail.com>
2024-11-03 19:24:30 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -23,7 +23,7 @@ class MyClient(discord.Client):
 
     @tasks.loop(seconds=2)  # Check every minute
     async def check_status(self):
-        member = self.Guild.get_member(USER_ID)
+        member = Guild.get_member(USER_ID)
         if member is not None:
             if self.first_check:
                 print(f'Checking status for {member.name}: {member.status}')
